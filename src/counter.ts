42a7ac8da62ea72edee8c9143388fbabee9dd057
algoritmi4 <ivan.butorin@fintopio.com>
2025-08-10 15:44:50 +0300

    feat: init linter, ts and prettier

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,10 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+export function setupCounter(element: HTMLButtonElement): void {
+  let counter = 0;
+  const setCounter: (count: number) => void = (count: number) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+
+  element.addEventListener('click', () => setCounter(counter + 1));
+  setCounter(0);
 }
